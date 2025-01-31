5340bab179b30235835fc0ebaa193fb6a01c5be0
Pramb13 <101867925+Pramb13@users.noreply.github.com>
2025-01-31 13:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,14 @@ def initialize_pinecone():
     
     # Fetch Pinecone environment from Streamlit secrets
     pinecone_environment = st.secrets["pinecone"]["environment"]  # Get environment from secrets
-
-    # Initialize Pinecone client
-    client = pinecone.Client(api_key=PINECONE_API_KEY, environment=pinecone_environment)
-
+    
+    # Initialize Pinecone
+    pinecone.init(api_key=PINECONE_API_KEY, environment=pinecone_environment)
+    
     # Access the Pinecone index
-    index = client.index(INDEX_NAME)
+    index = pinecone.Index(INDEX_NAME)
     return index
+
 
 # Store data in Pinecone
 def store_in_pinecone(index, image, predicted_class_idx, prediction_score):
